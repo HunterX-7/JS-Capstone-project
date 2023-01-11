564a0f5de683cdbf1283fe1bb6031ec178eee656
Rashed Arman <rashedarman21@gmail.com>
2023-01-12 00:30:58 +0600

    Involvement API addLike method

---
+++ src/api/likesApi.js
@@ -0,0 +1,10 @@
+import { APP_ID } from '../config';
+
+const baseUrl = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${APP_ID}/likes`;
+
+export const addLike = (itemId) =>
+  fetch(baseUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ item_id: itemId }),
+  }).then((response) => response.ok);
--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
+export const APP_ID = 'sMqOal4h14UBvcJsICcu';
 export const CLIENT_ID = '';
 export const CLIENT_SECRET = '';
