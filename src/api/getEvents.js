c1b6cdf5239ce52ec527ca99cda3e045d22b7f90
Rashed Arman <rashedarman21@gmail.com>
2023-01-11 14:12:21 +0600

    Implement API get events

---
+++ src/api/getEvents.js
@@ -0,0 +1,13 @@
+import fetchWithAuth from './fetchWithAuth.js';
+
+const getEvents = async () => {
+  const response = await fetchWithAuth('events');
+  if (!response.ok) {
+    throw new Error(
+      `HTTP error! status: ${response.status} ${response.statusText}`
+    );
+  }
+  return await response.json();
+};
+
+export default getEvents;
