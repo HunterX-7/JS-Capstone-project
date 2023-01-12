ff620b17e54a48ac598047c9a6e6faed136ec437
Rashed Arman <rashedarman21@gmail.com>
2023-01-12 22:45:47 +0600

    Format line braks using eslint

--- src/api/likesApi.js
+++ src/api/likesApi.js
@@ -2,12 +2,10 @@ import { APP_ID } from '../config.js';
 
 const baseUrl = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${APP_ID}/likes`;
 
-export const addLike = (itemId) =>
-  fetch(baseUrl, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ item_id: itemId }),
-  }).then((response) => response.ok);
+export const addLike = (itemId) => fetch(baseUrl, {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ item_id: itemId }),
+}).then((response) => response.ok);
 
-export const getLikes = () =>
-  fetch(baseUrl).then((response) => response.json());
+export const getLikes = () => fetch(baseUrl).then((response) => response.json());
