0d64e1ae3c5f77b9252b46bec5791006cf862544
Israel CHIZUNGU <chizunguisrael@gmail.com>
2023-01-12 19:27:37 +0300

    Create dynamic popup

--- src/modules/showEvents.js
+++ src/modules/showEvents.js
@@ -10,7 +10,7 @@ const createEventCard = (event) => {
   } = event;
 
   const html = `
-    <div class="card" data-card=${id}>
+    <div class="card" data-card=${id} id="${id}">
       <img class="card-image" src="${performerImage}" alt="${performerName}">
 
       <div class="card-title-container">
@@ -24,7 +24,7 @@ const createEventCard = (event) => {
       </div>
 
       <div class="card-button-container">
-          <button class="btn btn-primary-outline">Comments</button>
+          <button class="btn btn-primary-outline comment-btn">Comments</button>
           <button class="btn btn-primary">Reserve</button>
       </div>
     </div>
@@ -40,5 +40,98 @@ const showEvents = async () => {
     eventsListEl.innerHTML += createEventCard(event);
   });
 };
+
+const previewBtn = document.querySelectorAll('.comment-btn');
+let wrapper = null;
+
+function showModal(obj){
+
+  const {
+    id,
+    short_title: shortTitle,
+    performers: [{ name: performerName, image: performerImage }],
+  } = obj;
+
+  if (wrapper !== null) {
+    document.body.removeChild(wrapper);;
+  }
+  // clicked
+  wrapper = document.createElement('div');
+  wrapper.innerHTML = `
+  <div class="modal" id="modal">
+    <div class="modal-content">
+      <div class="card-preview-header">
+        <h3>Event Details</h3>
+        <span class="close">&times;</span>
+      </div>
+
+      <img src=${performerImage}
+        class="popup-img-preview" alt="Image cover">
+      <h2 class="work-title-preview">${shortTitle}</h2>
+      <div class="description-popup">
+        <div class="primaryInfo">
+          <h3>Date : 23/10/2019</h3>
+          <h3>Ticket : 120 USD</h3>
+        </div>
+        <div class="detailInfo">
+          <h3>Time: 7 PM</h3>
+          <h3>Notice: Wear your mask</h3>
+        </div>
+      </div>
+      <div class="article-content">
+        <div class="comments-container">
+          <div class="comment-list">
+            <h3>Comments (4)</h3>
+            <!-- comments -->
+            <h4 class="comment-item">Comment 1 : "Welcome dear"</h4>
+            <h4 class="comment-item">Comment 1 : "Welcome dear"</h4>
+            <h4 class="comment-item">Comment 1 : "Welcome dear"</h4>
+          </div>
+          <hr>
+          <form action="" id="add-comment">
+            <h2>Add Comment</h2>
+            <label for="">Your name</label>
+            <div>
+              <input type="text" name="name" required>
+            </div>
+            <label for="">Your insights</label>
+            <div>
+              <textarea name="insights" id="" cols="30" rows="3"></textarea>
+            </div>
+            <input type="submit" name="comment" id="comment-btn" value="Post your Comment">
+          </form>
+        </div>
+      </div>
+
+    </div>
+  </div>`;
+  document.body.appendChild(wrapper);
+
+  const modal = document.getElementById('modal');
+  modal.style.display = 'block';
+  // // Get the <span> element that closes the modal
+  const span = document.getElementsByClassName('close')[0];
+
+  // // When the user clicks on <span> (x), close the modal
+  span.onclick = () => {
+    modal.style.display = 'none';
+  };
+
+  window.onclick = (event) => {
+    if (event.target === document.getElementById('modal')) {
+      modal.style.display = 'none';
+    }
+  };
+}
+
+
+previewBtn.forEach((node) => {
+  document.getElementById(node.id).addEventListener('click', async () => {
+    // pass objects to the modal
+    const { event } = await getEvents();
+    showModal(event);
+  });
+});
+
 
 export default showEvents;
