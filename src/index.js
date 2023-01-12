d1b4a8f915cdcc13f662a2d9590344439e6102c4
HunterX-7 <fraquin7@gmail.com>
2023-01-12 15:31:27 -0500

    Add the popup to generate dynamically

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import showEvents from './modules/showEvents.js';
 import './styles/reset.css';
 import './styles/style.css';
+import showReservation from './modules/showReservation.js';
 
 document.addEventListener('DOMContentLoaded', showEvents);
+
+const bodyElem = document.getElementById('body');
+const footerElem = document.getElementById('footer');
+const popupReser = showReservation();
+bodyElem.insertBefore(popupReser, footerElem);
