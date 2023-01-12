f74298e4eb1a03e65a44692e3e16039bde034b29
Rashed Arman <rashedarman21@gmail.com>
2023-01-12 21:25:47 +0600

    Show events count to DOM

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import showEvents from './modules/showEvents.js';
+import showEventsCount from './modules/showEventsCount.js';
 import './styles/reset.css';
 import './styles/style.css';
 
-document.addEventListener('DOMContentLoaded', showEvents);
+document.addEventListener('DOMContentLoaded', () => {
+  showEvents().then(() => {
+    showEventsCount();
+  });
+});
